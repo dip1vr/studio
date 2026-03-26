55d5bd922532c2bc58d7b0556e49c56742999234
Dip <diphype@gmail.com>
2026-03-26 16:22:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7121072959-1c750",
+  "appId": "1:935688303182:web:e7fd60872e6a95fa32491f",
+  "apiKey": "AIzaSyATVTTpH4C7FIexmgxEsrxfLhemk1ckUGU",
+  "authDomain": "studio-7121072959-1c750.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "935688303182"
+};
